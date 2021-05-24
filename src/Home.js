4b8cb1af397a3e33241a5d5e2727b8205dfb8a33
Bikash Karmokar <b.karmokar@innosep.de>
2021-05-24 17:32:19 +0200

    added post action usign axios

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,50 @@
 import React from "react";
 import {Component} from "react";
+import axios from 'axios';
 
 export class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {welcome: ''};
+    state = {
+        corona_prediction: {
+            corona: 0
+        }
     }
-    callApi() {
-        var self = this
-        // Call the API page
-        fetch('http://127.0.0.1:8000/welcome/')
-            .then((result) => {
-                // Get the result
-                // If we want text, call result.text()
-                return result.json();
-            }).then((jsonResult) => {
-            // Do something with the result
-            console.log(jsonResult);
-            self.state.welcome = jsonResult
 
-        })
+    user_symptoms = {
+        'personal_info': {
+            'name': 'Mr. X',
+            'email': 'x@cov.com',
+            'mobile': '+4925457459745'
+        },
+        'symptoms': {
+            "cough": [1],
+            "fever": [1],
+            "sore_throat": [1],
+            "shortness_of_breath": [1],
+            "head_ache": [1],
+            "age_60_and_above": [1],
+            "gender": [1],
+            "test_indication": [1]
+        }
+    }
+
+    getWelcomeMessage = event => {
+
+        axios.get('http://127.0.0.1:8000/welcome/')
+            .then(response => {
+                console.log(response.data);
+            })
+    }
+
+    getCovidPredictionResult = event => {
+
+        const user_symptoms = this.user_symptoms
+
+        axios.post('http://127.0.0.1:8000/predict/', user_symptoms)
+            .then(response => {
+                // console.log(response.data);
+                this.setState({corona_prediction: response.data})
+                console.log(this.state.corona_prediction)
+            })
     }
 
     render() {
@@ -29,13 +54,19 @@ export class Home extends Component {
 
                     <input className="mt-md-0 d-flex justify-content-left"
                         name="response"
-                        type="text" value={this.state.value}
+                        type="text" value={this.state.corona_prediction['corona']}
                     />
                 </label>
 
+                 <button type="submit"
+                        className="btn btn-primary"
+                        onClick={() => this.getWelcomeMessage()}>
+                    Welcome
+                </button>
+
                 <button type="submit"
                         className="btn btn-primary"
-                        onClick={() => this.callApi()}>
+                        onClick={() => this.getCovidPredictionResult()}>
                     Predict
                 </button>
 
